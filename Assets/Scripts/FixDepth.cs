cc5a0126cc2844f15ce38c0a9bbeeee6fe22ee96
idearcos <idearcos@users.noreply.github.com>
2018-02-11 17:44:13 +0900

    Improved NewPlayer -Added NewPlayer script to capture movement and update animator. Added box collider and rigid body components to NewPlayer too. -Fixed idle animations: added one more event after 1 frame, otherwise the duration of the animation was considered 0.00s even if there was an event that needed to be triggered. -Made some adjustments in Animator to match the behavior of Player in terms of animations (since these animations based on Events seem to behave somewhat differently). -Added offset property to FixDepth script, so that different layers of LPC characters will be rendered in the right order after running the script's Update method.

--- Assets/Scripts/FixDepth.cs
+++ Assets/Scripts/FixDepth.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class FixDepth : MonoBehaviour {
 
 	public bool fixEveryFrame;
+	public int offset = 0;
 	SpriteRenderer sprite;
 
 	// Use this for initialization
 	void Start () {
 		sprite = GetComponent<SpriteRenderer> ();
 		sprite.sortingLayerName = "Player";
-		sprite.sortingOrder = Mathf.RoundToInt (-transform.position.y * 100);
+		sprite.sortingOrder = Mathf.RoundToInt (-transform.position.y * 100) + offset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (fixEveryFrame) {
-			sprite.sortingOrder = Mathf.RoundToInt (-transform.position.y * 100);
+			sprite.sortingOrder = Mathf.RoundToInt (-transform.position.y * 100) + offset;
 		}
 	}
 }
---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : MonoBehaviour {
+
+	public float speed = 4f;
+
+	Vector2 mov;
+	Animator animator;
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CaptureMovement ();
+		UpdateAnimation ();
+
+	}
+
+	void FixedUpdate() {
+		rb2d.MovePosition (rb2d.position + mov * speed * Time.deltaTime);
+	}
+
+	void CaptureMovement() {
+		mov = new Vector2 (
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical")
+		);
+	}
+
+	void UpdateAnimation() {
+		if (mov != Vector2.zero) {
+			animator.SetFloat ("movX", mov.x);
+			animator.SetFloat ("movY", mov.y);
+			animator.SetBool ("walking", true);
+		} else {
+			animator.SetBool ("walking", false);
+		}
+	}
+}
