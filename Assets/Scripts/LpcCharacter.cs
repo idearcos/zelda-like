a5f7c84357f730b23a9cffab65b48c20280f17f2
idearcos <idearcos@users.noreply.github.com>
2018-02-11 16:40:09 +0900

    Added NewPlayer character, made up of LPC sprites -NewPlayer is started as an empty component, then all LPC sprites are added as child objects to it. -NewPlayer contains an Animator component. Animations in it have Events that call the UpdateSprites function in LpcCharacter script. -LpcCharacter script forwards the call to every LpcSprite component in the child objects. Then these update the sprite in their SpriteRenderer component. -Animations in NewPlayer use the index of each animation frame, which is common to every LPC sprite sheet, to drive the actual graphic animations. -LpcSprite scripts have a public Texture2D property, which they use to load every Sprite resource in the Start function.

---
+++ Assets/Scripts/LpcCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LpcCharacter : MonoBehaviour {
+
+	public void UpdateSprites(int index) {
+		foreach (LpcSprite lpcSprite in GetComponentsInChildren<LpcSprite>()) {
+			lpcSprite.UpdateSprite (index);
+		}
+	}
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+}
---
+++ Assets/Scripts/LpcSprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEditor;
+
+public class LpcSprite : MonoBehaviour {
+
+	public Texture2D spriteSheet;
+	SpriteRenderer spriteRenderer;
+	Sprite[] sprites;
+
+	public void UpdateSprite(int index) {
+		spriteRenderer.sprite = sprites [index];
+	}
+
+	void Awake() {
+		Assert.IsNotNull (spriteSheet);
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Remove the directories before "Resources", as well as the extension of the file
+		string spriteSheetPath = AssetDatabase.GetAssetPath (spriteSheet);
+		sprites = Resources.LoadAll<Sprite> (spriteSheetPath.Substring(17, spriteSheetPath.Length - 21));
+
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+}
